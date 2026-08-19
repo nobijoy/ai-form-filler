17ba3e03f2ea6aa0235983d8bef68e26b5753a79
nobijoy <nobijoy1446@gmail.com>
2026-08-19 17:03:48 +0600

    chore: Bump version to 1.0.2 in manifest, package.json, and package-lock.json; ensure consistency across project files for the new release.

--- manifest.config.ts
+++ manifest.config.ts
@@ -5,7 +5,7 @@ export default defineManifest({
   manifest_version: 3,
   name: "__MSG_extName__",
   description: "__MSG_extDescription__",
-  version: "1.0.1",
+  version: "1.0.2",
   default_locale: "en",
   icons: {
     16: "icons/icon16.png",
